9d12acc99cfd04a72ca760b4d02f64475208890c
hasanraza38 <92hasanraza689@gmail.com>
2024-05-14 03:24:50 +0500

    mobile-app

---
+++ app.js
@@ -0,0 +1,110 @@
+
+const phones = [
+    {
+        brand: 'Samsung',
+        model: 'Galaxy S20',
+        ram: 8,
+        rom: 128,
+        camera: '64MP Triple Camera',
+        price: 'PKR 154,999'
+    },
+    {
+        brand: 'Xiaomi',
+        model: 'Redmi Note 10',
+        ram: 4,
+        rom: 128,
+        camera: '48MP Quad Camera',
+        price: 'PKR 31,999'
+    },
+    {
+        brand: 'Infinix',
+        model: 'Infinix Zero 8',
+        ram: 8,
+        rom: 128,
+        camera: '64MP Quad Camera',
+        price: 'PKR 35,999'
+    },
+    {
+        brand: 'Tecno',
+        model: 'Tecno Camon 16 Premier',
+        ram: 8,
+        rom: 128,
+        camera: '64MP Quad Camera',
+        price: 'PKR 39,999'
+    },
+    {
+        brand: 'Apple',
+        model: 'iPhone 13',
+        ram: 6,
+        rom: 128,
+        camera: '12MP Dual Camera',
+        price: 'PKR 194,999'
+    },
+    {
+        brand: 'Oppo',
+        model: 'Oppo F19 Pro',
+        ram: 8,
+        rom: 256,
+        camera: '48MP Quad Camera',
+        price: 'PKR 49,999'
+    },
+    {
+        brand: 'Vivo',
+        model: 'Vivo Y51',
+        ram: 8,
+        rom: 128,
+        camera: '48MP Triple Camera',
+        price: 'PKR 39,999'
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy S21 Ultra',
+        ram: 12,
+        rom: 256,
+        camera: '108MP Quad Camera',
+        price: 'PKR 229,999'
+    }
+];
+
+const div = document.getElementById('div')
+
+function rendertems() {
+    for (let i = 0; i < phones.length; i++) {
+
+        // console.log(phones[i]);
+    
+        div.innerHTML +=`
+        <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${phones[i].brand} ${phones[i].model}</h5>
+                  <h6 class="card-title">${phones[i].price}</h6>
+    
+                  <p class="card-text">storage</p>
+                 
+                  <div>
+    
+                  <p class="card-text">${phones[i].rom}-${phones[i].ram}</p>
+                  
+                  </div>
+                  <button onclick='addToCart(${i})' type="button" class="btn btn-primary">add to cart</button>
+
+                </div>
+              </div>`
+        
+    
+        
+    }
+    
+    
+}
+rendertems()
+
+function addToCart(index) {
+if (phones.includes(phones[index])) {
+   console.log(phones[index].quantity += 1); 
+}
+else{
+console.log(index);
+
+}
+}
